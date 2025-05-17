04fc12d1702e82553889270e59ed5fac8a16282d
1stMorphine <monoshov.gg@gmail.com>
2025-05-17 21:32:05 +0200

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7200315610:AAH70ueIJUl6_LTPSFXkswRcwmh8o5HhPRs")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 GITHUB_URL = "https://github.com/YOUR_USERNAME/YOUR_REPO"
